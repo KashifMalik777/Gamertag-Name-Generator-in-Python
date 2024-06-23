ad3f91454dcbf1778bdb2e4112928ae3e5ef1ab7
Kashif Malik <md.kashif123490@gmail.com>
2024-06-23 16:56:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#Gamertag Name Generator Python Program
+print("Welcome to the Gamertag Name Generator")
+
+color = input("What is your favourite color?\n")
+
+car = input("What is your favourite car?\n")
+
+age = input("What is your age?\n")
+
+print("Your Gamertag name could be " + color + "_" + car + "_" + age)
